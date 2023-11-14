588a0ee5683c0e7fefe6c95420e50063cc940564
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-11-15 04:07:25 +0600

    add server for hat-bazar-client and create apis for save user and get user

--- index.js
+++ index.js
@@ -2,18 +2,13 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.og57wk2.mongodb.net/?retryWrites=true&w=majority`;
 
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json());
-
-// json data
-const collegeData = require("./jsonData/college-data.json");
-const reviewsData = require("./jsonData/reviews.json");
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.og57wk2.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,47 +21,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    /** data collection */
-    const reviewCollection = client
-      .db("collegeBookingCommerce")
-      .collection("reviewsData");
-    const collegeCollection = client
-      .db("collegeBookingCommerce")
-      .collection("collegesData");
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    const usersCollection = client.db("hat-bazar-next").collection("users");
 
-    /** implement search system */
-    // creating index on two fields
-    const indexKeys = { college_name: 1 };
-    const indexOptions = { name: "collegeName" };
-    const result = await collegeCollection.createIndex(indexKeys, indexOptions);
-
-    app.get("/searchCollege/:text", async (req, res) => {
-      const searchText = req.params.text;
-      const result = await collegeCollection
-        .find({
-          $or: [{ college_name: { $regex: searchText, $options: "i" } }],
-        })
-        .toArray();
+    // get user role form db
+    app.get("/users-role/:email", async (req, res) => {
+      const query = { email: req.params.email };
+      if (!query) {
+        return res.status(403).send({ error: true, message: "No user found" });
+      }
+      const result = await usersCollection.findOne(query);
       res.send(result);
     });
 
-    /**  college data route api */
-    app.get("/colleges", async (req, res) => {
-      const result = await collegeCollection.find().toArray();
-      res.send(result);
-    });
-
-    app.get("/colleges/:id", async (req, res) => {
-      const filter = { _id: new ObjectId(req.params.id) };
-      const result = await collegeCollection.findOne(filter);
-      res.send(result);
-    });
-
-    /** reviews data route api */
-    app.get("/reviews", async (req, res) => {
-      const result = await reviewCollection.find().toArray();
+    // save user email and role in db
+    app.put("/set-users/:email", async (req, res) => {
+      const user = req.body;
+      const query = { email: req.params.email };
+      const updateDoc = { $set: user };
+      const options = { upsert: true };
+      const result = await usersCollection.updateOne(query, updateDoc, options);
       res.send(result);
     });
 
@@ -83,9 +56,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("College booking server is sitting");
+  res.send("Hat Bazar Server is sitting");
 });
 
 app.listen(port, () => {
-  console.log(`College booking server is on port ${port}`);
+  console.log(`Hat-Bazar Server is on port: ${port}`);
 });
